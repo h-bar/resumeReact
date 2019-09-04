33ecced668b15e1797450de016da358554291dc9
Yan Zhang <663yan@gmail.com>
2019-09-03 23:18:27 -0400

    use firebase for resume data

--- src/index.js
+++ src/index.js
@@ -1,110 +1,98 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+import { unstable_createResource } from 'react-cache';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMobileAlt, faEnvelope, faMapMarkerAlt, faGraduationCap, faFolderOpen, faSuitcase } from '@fortawesome/free-solid-svg-icons'
+import { faMobileAlt, faEnvelope, faMapMarkerAlt, faGraduationCap, faFolderOpen, faSuitcase, faTimes, faTools } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
+import { Form, Field } from 'react-final-form';
+import { TextField, Checkbox, Radio, Select } from 'final-form-material-ui';
+
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+import arrayMutators from 'final-form-arrays'
+import { FieldArray } from 'react-final-form-arrays'
+
+import {
+  Typography,
+  Paper,
+  Link,
+  Grid,
+  Button,
+  CssBaseline,
+  RadioGroup,
+  FormLabel,
+  MenuItem,
+  FormGroup,
+  FormControl,
+  FormControlLabel,
+  IconButton,
+} from '@material-ui/core';
+
+
+import firebase from 'firebase';
+import 'firebase/firestore'
 import './index.scss';
 
-class resumeData {
-  info = {
-    firstname: 'Yan',
-    lastname: 'Zhang',
-    phone_num: '+1 (925)494-3211',
-    emails: ['663yan@gmail.com', 'yz2626@cornell.edu'],
-    linkedin: '663yan',
-    links: [],
-    address: '107 E State St, Ithaca, NY',
 
-    summary: '',
-    objective: ''
-  }
+const educationHeading = (education) => (
+  <div className='info'>
+    <div className="info_left">
+      <div className='info_top'>{education.institute}</div>
+      <div className='info_buttom'>{education.degree + ' in ' + education.major}</div>
+    </div>
+    <div className="info_right">
+      <div className='info_top'>
+        <FontAwesomeIcon icon={faMapMarkerAlt}/>
+        {education.location}
+      </div>
+      <div className='info_buttom'>{education.gpa}</div>
+    </div>
+  </div>
+)
 
-  educations = [
-    {
-      institute: 'Cornell University',
-      location: 'Ithaca, NY',
-      start_date: 'Aug 2019',
-      end_date: 'May 2020',
-      degree: 'Master of Engineering',
-      major: 'Electrical and Computer Engineering',
-      gpa: '',
-      description: '',
-      bulletpoints: []
-    },
-    {
-      institute: 'Rensselaer Polytechnic Institute (RPI)',
-      location: 'Troy, NY',
-      start_date: 'Aug. 2014',
-      end_date: 'May. 2019',
-      degree: 'Bachelor of Science',
-      major: 'Computer and System Engineering & Electrical Engineering',
-      gpa: '3.5/4.0',
-      description: '',
-      bulletpoints: []
-    }
-  ]
+const experienceHeading = (experience) => (
+  <div className='info'>
+    <div className="info_left">
+      <div className='info_top'>{experience.position}</div>
+      <div className='info_buttom'>{experience.organization}</div>
+    </div>
+    <div className="info_right">
+      <div className='info_top'>
+        <FontAwesomeIcon icon={faMapMarkerAlt}/>
+        {experience.location}
+      </div>
+      <div className='info_buttom'>{experience.skills}</div>
+    </div>
+  </div>
+)
 
-  experiences = [
-    {
-      orgnization: 'Hasbro',
-      location: 'Pawtucket, RI',
-      position: 'Electronic Engineer Intern',
-      start_date: 'Jan 2018',
-      end_date: 'Jul 2018',
-      description: '',
-      bulletpoints: [
-        'Built 10+ toy prototypes by bare-metal implementations on 8-bit microcontrollers interfacing with accelerometers, LEDs, and Speakers using vendor specific programming languages. Developed the firmware base on interruptions and debugged the design with benchtop tools',
-        'Evaluated the feasibility and difficulty of upgrading an EOL accelerometer. Built test board to assess different upgrading options and analyzed datasheets for potential issues. Filed a detailed report resulting in a 1-year extended supply for this accelerometer',
-        "Developed wrapper functions for microcontrollers' I2C and SPI interface. Handled timing critical conditions using assembly language",
-        'Created python scripts to automatically import sound libraries into firmware code'
-        ],
-      skills: '8-bit Microcontroller, Sensors, Soldering, Assembly, Python' 
-    },
-    {
-      orgnization: 'National Center for Adaptive Neurotechnologies (NCAN)',
-      location: 'Albany, NY',
-      position: 'Web Developer Intern',
-      start_date: 'May 2017',
-      end_date: 'Nov 2017',
-      description: '',
-      bulletpoints: [
-        'Constructed a RESTful API service using python-flask providing neurotechnology data analysis functionalities with Matlab scripts',
-        'Responsible for maintaining and optimizing two websites. Performed a DNS server switch. Organized data and data structure to reduce storage usage by 40%',
-        ],
-      skills: 'Python, RestFul API, Drupal, Matlab, Google API' 
-    },
-    
-    {
-      orgnization: 'National Center for Adaptive Neurotechnologies (NCAN)',
-      location: 'Albany, NY',
-      position: 'Web Developer Intern',
-      start_date: 'May 2017',
-      end_date: 'Nov 2017',
-    description: '',
-      bulletpoints: [
-        'Constructed a RESTful API service using python-flask providing neurotechnology data analysis functionalities with Matlab scripts',
-        'Responsible for maintaining and optimizing two websites. Performed a DNS server switch. Organized data and data structure to reduce storage usage by 40%',
-        ],
-      skills: 'Python, RestFul API, Drupal, Matlab, Google API' 
-    }
-  ]
+const projectHeading = (project) => (
+  <div className='info'>
+    <div className="info_left">
+      <div className='info_top'>{project.name}</div>
+      <div className='info_buttom'>{project.role}</div>
+    </div>
+    <div className="info_right">
+      <div className='info_top'>
+        {project.note}
+      </div>
+      <div className='info_buttom'>{project.skills}</div>
+    </div>
+  </div>
+)
 
-  projects = [{
-    name: '',
-    start_date: '',
-    end_date: '',
-    note: '',
-    description: '',
-    bulletpoints: [''],
-    skills: ''
-  }]
-
-  skills = [{
-    category: '',
-    skill: []
-  }]
-}
+const skillHeading = (skillset) => (
+  <div className='info'>
+    <div className="info_left">
+      <div className='info_top'>{skillset.category}</div>
+    </div>
+    <div className="info_right">
+      <div className='info_top'>{skillset.skills.join(', ')}</div>
+    </div>
+  </div>
+)
 
 class Heading extends React.Component {
   EmailList(emails) {
@@ -122,14 +110,14 @@ class Heading extends React.Component {
     return (
       <div className='heading'>
         <div id='name'>
-          <span id='firstname'>{this.props.info.firstname}</span>
+          <span id='firstname'>{this.props.info.first_name}</span>
           <span> </span>
-          <span id='lastname'>{this.props.info.lastname}</span>
+          <span id='lastname'>{this.props.info.last_name}</span>
         </div>
         <div id='contacts'>
           <span>
             <FontAwesomeIcon icon={faMobileAlt} />
-            <span id='phone_num'> {this.props.info.phone_num} | </span>
+            <span id='phone_number'> {this.props.info.phone_number} | </span>
           </span>
           {this.EmailList(this.props.info.emails)}
           <span>
@@ -142,73 +130,48 @@ class Heading extends React.Component {
   }
 }
 
-const educationHeading = (education) => (
-  <div className='info'>
-    <div className="info_left">
-      <div className='info_top'>{education.institute}</div>
-      <div className='info_button'>{education.degree + ' in ' + education.major}</div>
-    </div>
-    <div className="info_right">
-      <div className='info_top'>
-        <FontAwesomeIcon icon={faMapMarkerAlt}/>
-        {education.location}
-      </div>
-      <div className='info_button'>{education.gpa}</div>
-    </div>
-  </div>
-)
-
-const experienceHeading = (experience) => (
-  <div className='info'>
-    <div className="info_left">
-      <div className='info_top'>{experience.position}</div>
-      <div className='info_button'>{experience.orgnization}</div>
-    </div>
-    <div className="info_right">
-      <div className='info_top'>
-        <FontAwesomeIcon icon={faMapMarkerAlt}/>
-        {experience.location}
-      </div>
-      <div className='info_button'>{experience.skills}</div>
-    </div>
-  </div>
-)
-
-// const projectHeading = (project) => ()
-
-// const skillHeading = (skillset) => ()
-
-
 class Subsection extends React.Component {
   bpList(items) {
     return items.map((item, idx) => <li key={idx} className='bp'>{item}</li>)
   }
 
   render() {
+    let data = this.props.data
+
+    let dateDom = <div className="date"></div>
+    if (data.dates != null) {
+      dateDom = <div className="date">
+                  <span>{data.dates.end}</span>
+                  <span>{data.dates.start}</span>
+                </div>
+    }
+
+    let bpDom = null
+    if (data.details_list != null) {
+      bpDom = <div className="bps">
+                <ul>{this.bpList(data.details_list)}</ul>
+              </div>
+    }
+
     return (
       <div className='subsection'>
-        <div className="date">
-          <span>{this.props.info.end_date}</span>
-          <span>{this.props.info.start_date}</span>
-        </div>
+        {dateDom}
         <div className="decorator"></div>
         <div className="details">
-          {this.props.headingFn(this.props.info)}
+          {this.props.headingFn(data)}
           <div className='detail'>
-          <div className="description">{this.props.info.description}</div>
-          <div className="bps">
-            <ul>{this.bpList(this.props.info.bulletpoints)}</ul>
+            <div className="description">{data.description}</div>
+            {bpDom}
           </div>
         </div>
-      </div>
       </div>
     )
   }
 }
 
 class Section extends React.Component {
-  subsectionList(headingFn, items) {
-    return items.map((item) => <Subsection key={item.id} headingFn={headingFn} info={item}/>)
+  subsectionList(headingFn, data) {
+    return data.map((item) => <Subsection key={item.id} headingFn={headingFn} data={item}/>)
   }
   render() {  
     return (
@@ -218,30 +181,79 @@ class Section extends React.Component {
           <span > {this.props.title}</span>
         </div>
         <div className='subsections'>
-          {this.subsectionList(this.props.headingFn, this.props.info)}
+          {this.subsectionList(this.props.headingFn, this.props.data)}
         </div>
       </div>
     );
   }
 }
-  
+
 class Resume extends React.Component {
-  render () {
-    const resume = new resumeData()
+  render = () => {
+    const resume = this.props.resume
     return (
       <div className='resume'>
         <Heading info={resume.info}/>
-        <Section icon={faGraduationCap} title={'Educations'} headingFn={educationHeading} info={resume.educations} />
-        <Section icon={faSuitcase} title={'Experiences'} headingFn={experienceHeading} info={resume.experiences} />
-        {/* <Section icon={faFolderOpen} title={'Projects'} info={resume.projects} /> */}
+        <Section icon={faGraduationCap} title={'Educations'} headingFn={educationHeading} data={resume.educations} />
+        <Section icon={faSuitcase} title={'Experiences'} headingFn={experienceHeading} data={resume.experiences} />
+        <Section icon={faFolderOpen} title={'Projects'} headingFn={projectHeading} data={resume.projects} />
+        <Section icon={faTools} title={'Skills'} headingFn={skillHeading} data={resume.skillsets} />
       </div>
     );
   }
 }
 
-// ========================================
+const fetchResume = async () => {
+  let getResumeData = async (collection) => {
+    let res = await db.collection(collection).limit(1).get()
+  
+    let data = []
+    res.forEach((doc) => {
+      data.push(doc.data())
+    });
+  
+    return data
+  }
 
+  let resume = {}
+  resume.info = (await getResumeData('personal_info'))[0]
+  resume.educations = await getResumeData('educations')
+  resume.experiences = await getResumeData('experiences')
+  resume.projects = await getResumeData('projects')
+  resume.skillsets = await getResumeData('skillsets')
+
+  return resume
+}
+
+// ========================================
+const firebaseConfig = {
+  apiKey: "AIzaSyCoZfD861oMY6yhCMglf4yy1lSUohNGxgU",
+  authDomain: "resumebuilder-1fd38.firebaseapp.com",
+  databaseURL: "https://resumebuilder-1fd38.firebaseio.com",
+  projectId: "resumebuilder-1fd38",
+  storageBucket: "resumebuilder-1fd38.appspot.com",
+  messagingSenderId: "248096012054",
+  appId: "1:248096012054:web:f57dfa700f6191f8"
+};
+
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore()
+
+const main = async () => {
+  const resume = await fetchResume()
+  console.log(resume)
+  ReactDOM.render(
+    <div>
+      <Resume resume={resume} />
+    </div>,
+    document.getElementById('root')
+  )
+}
+
+main()
 ReactDOM.render(
-  <Resume />,
+  <div>
+    <div>loading...</div>
+  </div>,
   document.getElementById('root')
 );  
